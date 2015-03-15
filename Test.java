ce14509e4f3acc97beed52252beeee204e37fc64
Alan Cowap <AlanCowap@users.noreply.github.com>
2015-03-15 20:25:09 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 classTest{
   public static void main(String... args){
-    new Test().sayHello(args[0]);
+    if(0 < args.length)
+      new Test().sayHello(args[0]);
   }
   
   private void sayHello(String msg){
