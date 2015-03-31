5cc5ef951ee1bd436e8198dfe9b3c63bb980d14c
Alan Cowap <AlanCowap@users.noreply.github.com>
2015-03-31 11:20:59 +0100

    Create RegNumber.java
    Class representing Registration Numbers for vehicles

---
+++ RegNumber.java
@@ -0,0 +1,11 @@
+class RegNumber{
+  private int year;
+  private String countyCode;
+  private int number;
+  
+  RegNumber(int year, String countyCode, int number){
+    this.year = year;
+    this.countyCode = countyCode;
+    this.number = number;
+  }
+}
