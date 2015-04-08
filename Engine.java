100921918d5ab12d91cd59a60ff5e1e5e9287617
Alan Cowap <AlanCowap@users.noreply.github.com>
2015-04-08 17:06:30 +0100

    Add revs

--- Engine.java
+++ Engine.java
@@ -1,10 +1,17 @@
 class Engine{
   private float capacity
   private int maxRevs;
+  private int revs;
   
   Engine(float capacity, int maxRevs){
     this.capacity = capacity;
     this.maxRevs = maxRevs;
+  }
+  
+  public void setRevs(float revs){
+    if (revs <= maxRevs){
+      this.revs = revs;
+    }
   }
   
 }
